896ad56e66b7761ba7cb8276984c7443c6405328
Vikrant1567 <sahanideepa168@gmail.com>
2026-06-07 18:16:39 +0530

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCFs0kgwd6pY98OgST9Ayi2t3uIYgaAp2Q",
+  authDomain: "sadhikabirthday.firebaseapp.com",
+  projectId: "sadhikabirthday",
+  storageBucket: "sadhikabirthday.firebasestorage.app",
+  messagingSenderId: "411707646592",
+  appId: "1:411707646592:web:280d9316ff4818d2387c65"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
