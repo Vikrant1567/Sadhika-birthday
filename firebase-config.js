5b33b835de185d34ca48d6de77b7710893936bd1
Vikrant1567 <sahanideepa168@gmail.com>
2026-06-07 18:47:38 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,7 +1,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
 import {
-    getFirestore
+  getFirestore
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const firebaseConfig = {
@@ -15,17 +15,4 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 
-export const db = getFirestore(app);import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyCFs0kgwd6pY98OgST9Ayi2t3uIYgaAp2Q",
-  authDomain: "sadhikabirthday.firebaseapp.com",
-  projectId: "sadhikabirthday",
-  storageBucket: "sadhikabirthday.firebasestorage.app",
-  messagingSenderId: "411707646592",
-  appId: "1:411707646592:web:280d9316ff4818d2387c65"
-};
-
-const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
